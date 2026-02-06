a71cec4ccbdedb984812f6c8b44c4c1d1ccbc905
Alexandr2281 <bebenkovalexander@yandex.ru>
2026-02-06 14:39:30 +0300

    Add files via upload

---
+++ src/ConsoleApp1/PostBox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class PostBox
+    {
+        public List<Newspaper> newspapers = new List<Newspaper>();
+
+        // Добавление записей
+        public void AddBook(Newspaper news)
+        {
+            newspapers.Add(news);
+        }
+
+        // Сортировка
+        public void SortNews()
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < newspapers.Count - 1; i++)
+                {
+
+                    if (CompareBooks(newspapers[i], newspapers[i + 1]) > 0)
+                    {
+                        Swap(i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        // Сравнение элементов
+        private int CompareBooks(Newspaper first, Newspaper second)
+        {
+            int genreComparison;
+            if (first.ChisloStranic < second.ChisloStranic)
+            { genreComparison = -1; }
+            else
+            {
+                if (first.ChisloStranic == second.ChisloStranic) genreComparison = 0;
+                else genreComparison = 1;
+            }
+
+
+            if (genreComparison != 0)
+                return genreComparison;
+
+            return string.Compare(first.Nazvanie, second.Nazvanie, StringComparison.OrdinalIgnoreCase);
+
+        }
+
+        private void Swap(int indexA, int indexB)
+        {
+            var temp = newspapers[indexA];
+            newspapers[indexA] = newspapers[indexB];
+            newspapers[indexB] = temp;
+        }
+
+        // Сохранение в файл
+        public void SaveToFile(string filePath)
+        {
+            using StreamWriter writer = File.CreateText(filePath);
+            foreach (var news in newspapers)
+            {
+                writer.WriteLine($"{news.Nazvanie},{news.Vladelec},{news.ChisloStranic}");
+            }
+        }
+    }
+
+}
---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostBox postBox = new PostBox();
+
+
+            int n;
+            string StrN;
+        Vodn:
+            bool VodNChesla = false;
+
+            Console.Write("Введите количество записей: ");
+            string input = Console.ReadLine();
+
+            if (!IsValidInt(input))
+            {
+                Console.WriteLine("Ошибка: введено неверное значение.");
+
+                VodNChesla = true;
+            }
+            if (VodNChesla) goto Vodn;
+
+            n = Convert.ToInt32(input);
+
+            if (n <= 0)
+            {
+                Console.WriteLine("количество записей должно быть положительным числом");
+                VodNChesla = true;
+            }
+            if (VodNChesla) goto Vodn;
+
+
+            Console.Clear();
+
+            string Nazvanie, Vladelec, ChisloStranicStr;
+            int ChisloStranic;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"запись {i + 1}");
+                Console.Write("Введите название газеты: ");
+                Nazvanie = Console.ReadLine();
+
+
+                Console.Write("Введите владельца газеты: ");
+                Vladelec = Console.ReadLine();
+
+            ChisloStanic:
+
+                Console.Write("Введите число страниц газеты: ");
+                ChisloStranicStr = Console.ReadLine();
+
+
+
+
+
+                if (!IsValidInt(ChisloStranicStr))
+                {
+                    Console.WriteLine("Ошибка: введено неверное значение.");
+
+                    goto ChisloStanic;
+
+
+                }
+
+                ChisloStranic = Convert.ToInt32(ChisloStranicStr);
+
+                if (ChisloStranic <= 0)
+                {
+                    Console.WriteLine("число страниц должно быть положительным числом");
+
+                    goto ChisloStanic;
+
+                }
+
+
+
+
+                postBox.AddBook(new Newspaper { Nazvanie = Nazvanie, Vladelec = Vladelec, ChisloStranic = ChisloStranic });
+
+
+
+                Console.Write("\n");
+            }
+
+
+
+            postBox.SortNews();
+
+            postBox.SaveToFile("news.txt");
+
+
+
+        }
+
+        static public bool IsValidInt(string value)
+        {
+            try
+            {
+                // Проверяем преобразование в Int32
+                _ = Convert.ToInt32(value);
+                return true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+    }
+
+
+
+}
+
+public struct Newspaper
+{
+    public string Nazvanie { get; set; }
+    public string Vladelec { get; set; }
+    public int ChisloStranic { get; set; }
+}
